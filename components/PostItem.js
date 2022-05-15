22c6099d75aa648538084d2d99da268bc3965ba5
Yeo Swe Hou <swehou@gmail.com>
2022-05-16 02:08:49 +0900

    added post buttons

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,8 +1,41 @@
+import {
+  HeartIcon,
+  EmojiHappyIcon,
+  PaperAirplaneIcon,
+  ChatIcon,
+  BookmarkIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
-function PostItem() {
+function PostItem({ name, img, caption }) {
   return (
-    <div>PostItem</div>
-  )
+    <div className="bg-white my-7 border rounded-sm">
+      <div className="flex items-center p-5">
+        <img
+          src={img}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt=""
+        />
+        <p className="flex-1 font-semibold">{name}</p>
+        <DotsHorizontalIcon className="navBtn" />
+      </div>
+      <img src={img} className="object-cover w-full" alt="" />
+      <div className="flex justify-between px-4 py-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="postBtn" />
+          <ChatIcon className="postBtn" />
+          <PaperAirplaneIcon className="postBtn" />
+        </div>
+        <BookmarkIcon className="postBtn" />
+      </div>
+
+        <p className="p-5 truncate">
+            <span className="">{name}</span>
+        </p>
+
+    </div>
+  );
 }
 
-export default PostItem
+export default PostItem;
--- components/Posts.js
+++ components/Posts.js
@@ -1,11 +1,28 @@
-import PostItem from "./PostItem"
+import PostItem from "./PostItem";
+
+const posts = [
+  {
+    id: "123",
+    username: "aeroaero",
+    userImg: "https://links.papareact.com/jjm",
+    caption: "aero is the best"
+  },
+  {
+    id: "123",
+    username: "aeroaero",
+    userImg: "https://links.papareact.com/jjm",
+    caption: "aero is the best"
+  },
+];
 
 function Posts() {
   return (
-      <div>
-          <PostItem/>
-      </div>
-  )
+    <div>
+    {posts.map((post)=> (
+        <PostItem key={posts.id} id={post.id} name={post.username} img={post.userImg} caption={post.caption}/>
+    ))}        
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
