97992dd749a937bdc935b49ee74be2531d8a94ed
Yeo Swe Hou <swehou@gmail.com>
2022-05-16 20:08:26 +0900

    modal component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Feed from "../../components/Feed";
-import Header
- from "../../components/Header";
+import Header from "../../components/Header";
+import Modal from "../../components/Modal";
 export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
@@ -11,9 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Feed */}
-      <Feed/>
-      {/* Modal */}
+      <Feed />
+      <Modal />
     </div>
   );
 }
